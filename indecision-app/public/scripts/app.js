9db5f2c309bb844994a16a6d28d883912668bcd5
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2018-11-21 17:34:48 +0000

    add react packages

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// JSX
+var template = React.createElement(
+  'p',
+  null,
+  'This is JSX from app.js'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
