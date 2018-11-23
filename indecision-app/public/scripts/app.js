5a19f57ef3d48aead9338e6d5637a690a5a58a67
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2018-11-23 17:36:50 +0000

    react conditionals

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,11 +1,72 @@
 'use strict';
 
 // JSX
+var app = {
+  title: 'The Daily Mail',
+  subTitle: 'Nick Cage wins oscar',
+  options: ['One', 'Two']
+};
+
+var getSubTitle = function getSubTitle() {
+  if (app.subTitle) {
+    return;
+  }
+};
+
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  'This is JSX from app.js'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subTitle && React.createElement(
+    'p',
+    null,
+    app.subTitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    (app.options && app.options.length) > 0 ? "Here are your options" : "No options"
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item 1'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item 2'
+    )
+  )
 );
+
+// let user = {
+//   name: 'Mike',
+//   age: 29,
+//   location: 'London',
+// };
+
+// const getLocation = (location) => {
+//   if (location) {
+//     return <p>Location: {location}</p>;
+//   }
+// };
+
+// const templateTwo = (
+//   <div>
+//     <h1>{user.name ? user.name : 'Annonymous'}</h1>
+//     {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+//     {getLocation(user.location)}
+//   </div>
+// );
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,50 @@
 
 // JSX
-var template = <p>This is JSX from app.js</p>;
-var appRoot = document.getElementById('app');
+const app = {
+  title: 'The Daily Mail',
+  subTitle: 'Nick Cage wins oscar',
+  options: ['One', 'Two']
+};
+
+var getSubTitle = () => {
+  if (app.subTitle) {
+    return ;
+  }
+};
+
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subTitle && <p>{app.subTitle}</p>}
+    <p>{(app.options && app.options.length) > 0 ? "Here are your options" : "No options"}</p>
+    <ol>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      
+    </ol>
+  </div>
+);
+
+// let user = {
+//   name: 'Mike',
+//   age: 29,
+//   location: 'London',
+// };
+
+// const getLocation = (location) => {
+//   if (location) {
+//     return <p>Location: {location}</p>;
+//   }
+// };
+
+// const templateTwo = (
+//   <div>
+//     <h1>{user.name ? user.name : 'Annonymous'}</h1>
+//     {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+//     {getLocation(user.location)}
+//   </div>
+// );
+
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
